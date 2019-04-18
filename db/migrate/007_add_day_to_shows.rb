c23c88b41b1fcb8a9d61a8933bb4104093bdd0b0
Andrew Waller <andrewhwaller@gmail.com>
2019-04-18 22:16:55 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_to_shows.rb
+++ db/migrate/007_add_day_to_shows.rb
@@ -3,6 +3,6 @@ class AddDayToShows < ActiveRecord::Migration[4.2]
   #a new migration, and then in the change method,
   #we make the change.
   def change
-    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
